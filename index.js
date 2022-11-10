70b81f1d285b8784dd8247f656e4eb30b37d00f0
fariharaihan <liafariha5@gmail.com>
2022-11-10 15:57:50 +0600

    server api get

--- index.js
+++ index.js
@@ -97,6 +97,12 @@ async function run() {
             const result = await serviceCollection.insertOne(service);
             res.send(result)
         })
+        // app.get('/services/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const servise = await serviceCollection.findOne(query);
+        //     res.send(servise)
+        // })
 
 
     }
